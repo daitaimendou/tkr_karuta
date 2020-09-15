f1326bf40f4cf679ca6c8ba5e4fe58880a4fd5c3
toratano <todatanoto@gmail.com>
2020-09-15 23:02:45 +0900

    vue-youtubeを利用して動画の再生処理を作成

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,34 +1,40 @@
 <template>
   <div class="quiz">
+    <YoutubePlayer/>
     <button v-on:click="get_random_playlist">プレイリストを取得</button>
     <div>{{playlist_result}}</div>
   </div>
 </template>
 
 <script>
+import YoutubePlayer from './YoutubePlayer.vue'
+
 export default {
-  data() {
+    components: {
+        YoutubePlayer
+    },
+    data() {
       return {
           playlist_result: 'first'
       }
-  },
-  methods: {
-    get_random_playlist: function () {
-        this.playlist_result = 'hugahuga'
-        var api_url = 'https://www.googleapis.com/youtube/v3/playlistItems'
-        this.$axios
-            .get(api_url, {
-                params: {
-                    part: 'id,snippet',
-                    playlistId: 'PLwL8OTDdmtk1CpllAwXsRx1CO8eIk_fcI',
-                    maxResults: 3,
-                    key: 'AIzaSyDFYvSWSlfrDeS-92coInYlUHpgPqbTSmY'
-                }
-            }).then(response => {
-                console.log(response);
-                this.playlist_result = response
-            })
+    },
+    methods: {
+        get_random_playlist: function () {
+            this.playlist_result = 'hugahuga'
+            var api_url = 'https://www.googleapis.com/youtube/v3/playlistItems'
+            this.$axios
+                .get(api_url, {
+                    params: {
+                        part: 'id,snippet',
+                        playlistId: 'PLwL8OTDdmtk1CpllAwXsRx1CO8eIk_fcI',
+                        maxResults: 3,
+                        key: 'AIzaSyDFYvSWSlfrDeS-92coInYlUHpgPqbTSmY'
+                    }
+                }).then(response => {
+                    console.log(response);
+                    this.playlist_result = response
+                })
+        }
     }
-  }
 }
 </script>
---
+++ src/components/YoutubePlayer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="youtube_player">
+        <youtube :video-id="videoId" ref="youtube"/>
+        <button class="btn btn-primary" @click="playVideo">play</button>
+        <button class="btn btn-primary" @click="stopVideo">stop</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            videoId: 'M7lc1UVf-VE'
+        }
+    },
+    methods: {
+        playVideo() {
+            this.player.playVideo()
+        },
+        stopVideo() {
+            this.player.stopVideo()
+        },
+    },
+    computed: {
+        player() {
+            return this.$refs.youtube.player
+        }
+    }
+}
+</script>
