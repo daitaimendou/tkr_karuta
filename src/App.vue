e6d7403b823085e844b9412c911444af51b012a9
daitaimendou <todatanoto@gmail.com>
2020-09-20 21:44:06 +0900

    フッターを別コンポーネントに切り出し

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,7 @@
     <div class="bg-white">
       <Header/>
       <Quiz/>
-      <div class="bg-red py-3 text-center">
-        <small class="text-white">
-          制作者 <a href="https://twitter.com/daitaimendou" class="text-white" target="_brank">@daitaimendou</a>
-           / 
-          <a href="https://shinchoku.net/notes/54268" class="text-white" target="_brank">ご意見はこちらまで</a>
-        </small>
-      </div>
+      <Footer/>
     </div>
   </div>
 </template>
@@ -17,12 +11,14 @@
 <script>
 import Header from './components/Header.vue'
 import Quiz from './components/Quiz.vue'
+import Footer from './components/Footer.vue'
 
 export default {
   name: 'App',
   components: {
     Header,
     Quiz,
+    Footer,
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="bg-red py-3 text-center">
+        <small class="text-white">
+          制作者 <a href="https://twitter.com/daitaimendou" class="text-white" target="_brank">@daitaimendou</a>
+           / 
+          <a href="https://shinchoku.net/notes/54268" class="text-white" target="_brank">ご意見はこちらまで</a>
+        </small>
+    </div>
+</template>
